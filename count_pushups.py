a622d085e1dfb311d8f0c288a126c0cf2d11ef75
Jack Eberhardt <jackeberhardt@jacks-mbp.lan>
2021-04-29 13:26:14 -0400

    applying lowpass filter

--- count_pushups.py
+++ count_pushups.py
@@ -5,21 +5,17 @@ import numpy as np
 import pylab as pl
 from scipy.signal import butter, freqz, filtfilt, firwin, lfilter
 
-# Parsing accelerometer data
+# Parsing accelerometer data (iphone)
 def pull_data(file_name):
     f = open(file_name + '.csv')
+    f.readline()
     xs = []
     timestamps = []
     for line in f:
         value = line.split(',')
-        #print(value[0])
-        #zyx
         timestamps.append(float(value[1]))
-        # r = 0
-        # for i in range(2, len(value)):
-        #     r += float(value[i]) ** 2
-        # rs.append(r)
-        xs.append(value[len(value)-1])
+
+        xs.append(float(value[len(value)-1]))
     return np.array(xs), np.array(timestamps)
 
 
@@ -31,21 +27,22 @@ signal, timestamps = pull_data(accel_file)
 
 
 #plotting raw data
-plt.figure(figsize=(10,5))
-plt.plot(timestamps, signal, 'r-',label='unfiltered')
-plt.title("Unfiltered Pushups Signal")
-pl.legend(loc='upper left')
-plt.xlabel("Time (s)")
-plt.ylabel("Height (10^-3 m)")
-plt.grid()
-plt.show()
+
+# plt.figure(figsize=(10,5))
+# plt.plot(timestamps, signal, 'r-',label='unfiltered')
+# plt.title("Unfiltered Pushups Signal")
+# pl.legend(loc='upper left')
+# plt.xlabel("Time (s)")
+# plt.ylabel("Height (10^-3 m)")
+# plt.grid()
+# plt.show()
 
 
 
 # low-pass filter
-order = 3
-fs = 10
-cutoff = 2  # desired cutoff frequency of the filter, Hz. MODIFY AS APPROPRIATE
+order = 5
+fs = 50
+cutoff = 1  # desired cutoff frequency of the filter, Hz. MODIFY AS APPROPRIATE
 nyq = 0.5 * fs
 normal_cutoff = cutoff / nyq
 lp_b, lp_a = butter(order, normal_cutoff, btype='low', analog=False)
@@ -57,7 +54,7 @@ lp_signal = filtfilt(lp_b, lp_a, signal)
 plt.figure(figsize=(10,5))
 # plt.plot(timestamps, signal, 'r-', label = 'unfiltered')
 plt.plot(timestamps, lp_signal, 'b-', label = 'processed')
-plt.plot(timestamps, signal, 'r-',label='unfiltered')
+#plt.plot(timestamps, signal, 'r-',label='unfiltered')
 
 
 
@@ -67,3 +64,6 @@ plt.xlabel("Time (s)")
 plt.ylabel("Height (10^-3 m)")
 plt.grid()
 plt.show()
+
+
+
