ceeccef2856a0a1108c09464283ca9acaab9fef7
Jack Eberhardt <jackeberhardt@jacks-mbp.lan>
2021-05-05 11:10:14 -0400

    more cleaning up, adding to README

--- README.md
+++ README.md
@@ -3,8 +3,24 @@ Jack Eberhardt, Sankalp Ramesh, Benjamin Xu
 Final Project for CS328
 
 
-Our idea for our final project has to do with pushups. Pushups are a common and fairly easy bodyweight exercise that can be performed by most anyone for general 
-fitness and working out their chest. What we would do would be to recognize pushups being done based on analysis of sensor data, count the amount of pushups done 
-(using a certain standardized measurement program designed by us) in a specific amount of time. We would could the amount of pushups performed and suggest to the 
-user a realistic exercise goal to increase the amount of pushups they can do in general. 
+Our idea for our final project has to do with pushups. Pushups are a common and fairly easy bodyweight exercise that can be performed by most anyone for general fitness and working out their chest. What we would do would be to recognize pushups being done based on analysis of sensor data, count the amount of pushups done (using a certain standardized measurement program designed by us) in a specific amount of time. We would could the amount of pushups performed and suggest to the user a realistic exercise goal to increase the amount of pushups they can do in general. 
+
+What we ended up doing was training a classifier and detecting different exercises and distinguishing between them. 
+
+Classification and prediction is done in classification.py, feature extraction is done in features.py, and our data is in the data folder. 
+
+We recorded multiple different isolated sessions of exercises and tasks, and concatenated them in all_data.csv. This has the multiple minutes of recorded data inside of it, with the end of each section marked with a comment. 
+
+We also used a few functions from util.py (from assignment 2 part 1) like slidingWindow. 
+
+
+
+
+
+
+
+
+
+
+
 
--- features.py
+++ features.py
@@ -1,14 +1,7 @@
 
   
 # -*- coding: utf-8 -*-
-"""
-This file is used for extracting features over windows of tri-axial accelerometer
-data. We recommend using helper functions like _compute_mean_features(window) to
-extract individual features.
-As a side note, the underscore at the beginning of a function is a Python
-convention indicating that the function has private access (although in reality
-it is still publicly accessible).
-"""
+
 
 import numpy as np
 import matplotlib
@@ -18,66 +11,48 @@ import scipy
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Calculate Mean X,Y,Z
-# -----------------------------------------------------------------------------
+# X,Y,X Mean
 def calculate_mean(window):
     return np.mean(window, axis=0)
 
 
 
-
-# ---------------------------------------------------------------------------
-#		                    Calculate Standard Deviation X,Y,Z
-# -----------------------------------------------------------------------------
+# X,Y,X Standard Deviation
 def calculate_std(window):
     return np.std(window, axis=0)
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Calculate StdDev of Magnitude
-# -----------------------------------------------------------------------------
+# Standard Deviation of Magnitude
 def std_magnitude(window):
     return [np.std(get_magnitude(window))]
 
 
 
-
-# ---------------------------------------------------------------------------
-#		                    Calculate Magnitude
-# -----------------------------------------------------------------------------
+# Magnitude
 def get_magnitude(window):
 
     magnitude = np.zeros(len(window))
-
     for i in range(len(window)):
         temp = (window[i][0]**2 + window[i][1]**2 + window[i][2]**2) ** 0.5
         magnitude[i] = temp
 
-    
     return magnitude
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Calculate Mean of Magnitude
-# -----------------------------------------------------------------------------
+# Mean of Magnitude
 def mean_magnitude(window):
     magnitude = get_magnitude(window)
     return [np.mean(magnitude)]
 
 
-# ---------------------------------------------------------------------------
-#		                    Calculate Variance
-# -----------------------------------------------------------------------------
+# X,Y,X Variance
 def calculate_variance(window):
     return np.var(window, axis=0)
 
 
-# ---------------------------------------------------------------------------
-#		                    FFT
-# -----------------------------------------------------------------------------
+# FFT
 def fft_feature_calculator(window):
     fft_arr = np.fft.rfft(get_magnitude(window))
     return [sum(fft_arr.astype(float))] # could also experiment with max()
@@ -85,9 +60,7 @@ def fft_feature_calculator(window):
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Count Peaks
-# -----------------------------------------------------------------------------
+# Peak Count
 def count_peaks(window):
     magnitude = get_magnitude(window)
     peaks = signal.find_peaks(magnitude)[0]
@@ -96,9 +69,7 @@ def count_peaks(window):
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Mean Peak Height
-# -----------------------------------------------------------------------------
+# Mean Peak Height
 def mean_peak_height(window):
     magnitude = get_magnitude(window)
     peaks = signal.find_peaks(magnitude)[0]
@@ -106,45 +77,26 @@ def mean_peak_height(window):
     if len(peaks) == 0:
         return [0]
     
-
     return [np.mean(peaks)]
 
 
 
 
-# ---------------------------------------------------------------------------
-#		                    Entropy
-# -----------------------------------------------------------------------------
+# Entropy
 def entropy_calculator(window):
     mag = get_magnitude(window)
     counts,bins = np.histogram(mag,20)
     counts+=1
-
     PA = counts / np.sum(counts, dtype=float)
     SA = -PA * np.log(PA)
     return [-np.sum(PA * np.log(PA),axis=0)]
 
 
 
-
-# helper function
-# def find_peaks(window):
-#     #peak_timestamps = []
-#     peaks = []
-#     for i in range(1, len(window)-1):
-#         if (window[i] > window[i-1]) and (window[i] > window[i+1]):
-#             #step_timestamps.append(timestamps[i])
-#             peaks.append(window[i])
-#     return peaks
-
-
 def extract_features(window):
-
 
     x = []
     feature_names = []
-
-
 
     x.append(calculate_mean(window))
     feature_names.append("x_mean")
@@ -167,7 +119,6 @@ def extract_features(window):
     x.append(mean_magnitude(window))
     feature_names.append("magnitude_mean")
 
-
     x.append(count_peaks(window))
     feature_names.append("magnitude peak count")
 
@@ -177,32 +128,9 @@ def extract_features(window):
     x.append(entropy_calculator(window))
     feature_names.append("entropy")
 
-    
+    x.append(fft_feature_calculator(window))
+    feature_names.append("fft")
 
-
-
-    # print(count_peaks(window))
-    # print(get_magnitude(window))
-    #print(window)
-
-    # x.append(mean_peak_height(window))
-    # feature_names.append("mean peak height of magnitude")
-
-
-    # x.append(mean_peak_distance(window))
-    # feature_names.append("mean peak distance of magnitude")
-
-
-    # for i in range(0,len(x)):
-    #     print(str(x[i]) + "\n\n")
-
-
-    # plt.figure(figsize=(10,5))
-    # plt.plot(get_magnitude(window), 'b-', label = 'filtered data')
-    # plt.xlabel("Time (s)")
-    # plt.ylabel("Acceleration (m/(s^2))")
-    # plt.grid()
-    # plt.show()
 
     feature_vector = np.concatenate(x, axis=0) # convert the list of features to a single 1-dimensional vector
 
